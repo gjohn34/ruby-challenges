cab6bf61f5033f756c7974b75a2e741b86d91d34
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-02-22 09:24:36 +1000

    morning challenge + BEASTMODE

--- 03_largest_number.rb
+++ 03_largest_number.rb
@@ -1,23 +1,27 @@
 # Write a method that will take two numbers,
 # and return whichever is the largest.
-#
-# Example:
-# largest_number(100,50) should return 100
-# largest_number(10,20) should return 20
-#
-# Check your solution by filling in the folowing table:
-# number_1 || number_2 || expected || actual
-# 
-# If your stuck try working togeather
-# 
+def largest_number(num1,num2)
+  if !num1.is_a?(Integer) or !num2.is_a?(Integer)
+    puts "Convert to integer first"
+  elsif num1 > num2
+    puts num1
+  elsif num2 < num1
+    puts num2
+  elsif num1 == num2
+    puts "Buddy, can't you tell those are both equal to each other?"
+  end
+end
+
+defined?()
+
+
 # Beast Mode:
 # If you complete the challenge, feel free to come up
 # with your own examples and test corner cases.
-# 
+#
 # Examples - what if both numbers are equal or are strings?
 
-def largest_number (number_1, number_2)
-  # Your code here
-end
-
 largest_number(100,50)
+largest_number(25,25)
+largest_number("five", 25)
+largest_number(30, "sixty")
