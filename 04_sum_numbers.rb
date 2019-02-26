328c75355fdd94b7d631ba4451a0ecde0f2e4512
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-02-26 11:04:52 +1000

    Fixed up tests, added Exception rule

--- 04_sum_numbers.rb
+++ 04_sum_numbers.rb
@@ -7,17 +7,18 @@ def sum_numbers(numbers) # new method that takes 1 parameter
   array_total = 0 #beginning the sum at clean 0
   if numbers == [] # when provided an empty array default return is 0
     return nil #changed return value to nil
-  end
-  for number in numbers #iterating through array
-    if number.class.superclass != Numeric #if provided a non-number eg. string
-      puts "Index #{numbers.index(number)} is not a number. Exluding #{number.class}."
-      break
-    else
+  else
+    for number in numbers #iterating through array
       array_total += number #then adding that number to the total
     end
+    return array_total
   end
-  return array_total #returning the total. for result use puts/print
+rescue Exception => e
+  puts e.class, "Invalid Input"
 end
+
+
+
 
 
 
@@ -27,7 +28,7 @@ end
 puts sum_numbers([1,1,1]) #should return 3
 puts sum_numbers([5,2,100,0,10]) #should return 117
 puts sum_numbers([1,2, "Hello"])
-puts sum_numbers([2,4, "Goodbye"])
+#puts sum_numbers([2,4, "Goodbye"])
 puts sum_numbers([])
 
 # Check your solution by running the tests:
--- tests/04_sum_numbers_test.rb
+++ tests/04_sum_numbers_test.rb
@@ -7,8 +7,11 @@ class LargestNumberTest < Test::Unit::TestCase
     assert_equal(117, sum_numbers([5,2,100,0,10]))
     assert_equal(290, sum_numbers([55,23,183,0,29]))
     assert_nil(sum_numbers([]))
-    assert_nothing_thrown do
-      sum_numbers([2,4, "Goodbye"])
+    assert_nothing_raised TypeError do
+      sum_numbers([1,2, "Goodbye"])
     end
+    #assert_nothing_thrown do
+    #  sum_numbers([2,4, "Goodbye"])
+  #end
   end
 end
