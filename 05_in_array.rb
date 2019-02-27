5768e95fa88e1620062eb79361c26c1d3ba7f6dc
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-02-27 10:37:11 +1000

    complete challenge, for some reason my second method failed to read. very sad

--- 05_in_array.rb
+++ 05_in_array.rb
@@ -1,6 +1,17 @@
 # Methods
+def in_array(string, array)
+  for element in array #looping through array
+    if string == element #if the string provided matches an element in array
+      return true #return true
+    end
+  end #after looping if there are no matches method will return false
+  return false
+end
 
-# Write a method called 'in_array' which will take 
+puts in_array("help", ["hello", "string", "help"])
+puts in_array("niope", ["hello", "string", "help"])
+
+# Write a method called 'in_array' which will take
 # two parameteres:
 # One as a string and one as an array of strings
 
@@ -12,12 +23,6 @@
 
 # Example:
 # "hello", ["hi","howdy","hello"] should return true.
-
-def in_array (needle, haystack)
-  # Your code here
-end
-
-
 # Beast Mode:
 
 # Redo the challenge but try solving it again in a different way.
