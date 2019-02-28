58baf0ef1e4f2a4b9bf9cc27e48187ed35d64a8c
Glen Johnson <glenjohnson39@yahoo.com.au>
2019-02-28 10:11:06 +1000

    Completed challenges, had to declare global variables, will fix in future

--- time_and_bar.rb
+++ time_and_bar.rb
@@ -6,19 +6,53 @@
 # - Print this complete sentence
 
 
+def travel_to_class
+  time_to_class = 0
+  puts "My first step is to drive fifteen minutes to my most convenient bus stop where there is always a bus waiting,"
+  time_to_class += 15
+
+  print "then my partner and I usually sit on the bus for another 15 minutes and play on our phones. \n"
+  time_to_class += 15
+
+  puts "After that we say our goodbyes and I walk to the nearby loop bus, that can take a good five minutes and if I'm lucky the bus is just about ready to leave,"
+  time_to_class += 5
+
+  print "then you guessed it, another wait; but this one is only five minutes. \n"
+  time_to_class += 5
+
+  puts "Final step (pun not intended) is to walk another 5 to the IES building. \n"
+  time_to_class += 5
+
+  puts "All in all, my total travel time is #{time_to_class}."
+end
+
+#travel_to_class
+
 # PART 2:
-# You are working at a bar where you have a current backlog of drinks to make:
-# 3 cocktails
-# 2 waters
-# and
-# 6 beers
 
-# Write a program that asks the customer for their order.
-#   if they order a cocktail, add one to the number of cocktails you need to make,
-#   if they order a water, add one to the number of waters you need to make,
-#   if they order a beer, add one to the number of beers you need to pour
+#have to declare backlog as global for now. putting inside the order method resets
+#the total order amounts. Maybe make a class and instance within?
+$backlog = Hash.new
+$backlog["cocktail"] = 3
+$backlog["water"] = 2
+$backlog["beer"] = 6
 
-# Print the final drinks order so you know what to make
+def order
+  puts "Whaddya want? You only got 3 options: Beer, Cocktail or Water."
+  input = gets.chomp.downcase
+  case input
+  when "cocktail"
+    $backlog["cocktail"] += 1
+  when "water"
+    $backlog["water"] += 1
+  when "beer"
+    $backlog["beer"] += 1
+  else
+    puts "We don't make that here!"
+  end
+end
+
+order
 
 
 # Part Three:
@@ -27,3 +61,24 @@
 # Water sell for $6, and cost $0.15 to make
 
 # Print out the total profit for the orders you have
+total_cost = 0
+total_sell = 0
+products = Hash.new
+products = {"Cocktail" => {cost: 8, sell: 22, backlog: 3}, "Beer" => {cost: 3, sell: 12, backlog: 6}, "Water" => {cost: 6, sell: 0.15, backlog: 6}}
+for drink_name, info in products
+  for price_point, amount in info
+    if price_point == :cost
+      temp_cost = amount * info[:backlog]
+      total_cost += temp_cost
+    elsif price_point == :sell
+      temp_sell = amount * info[:backlog]
+      total_sell += temp_sell
+    end
+  end
+end
+
+puts "Total Cost: #{total_cost}"
+puts "Total Sell: #{total_sell}"
+profit = total_sell - total_cost
+puts "Total Profit: #{profit.round(2)}"
+#profit = sell - cost
